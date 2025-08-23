80695367a8ef613608cdd0e1ba24ac48cebde59e
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-08-24 01:31:38 +0800

    chore: switch to JS config for lint-staged config

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,12 +1,14 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const path = require('path')
+import path from 'path'
 
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
     .join(' --file ')}`
 
-module.exports = {
+/** @type {import('lint-staged').Configuration} */
+const config = {
   '!(*.{js,mjs,jsx,ts,tsx})': 'prettier --write --ignore-unknown',
   '*.{js,mjs,jsx,ts,tsx}': [buildEslintCommand, 'prettier --write'],
 }
+
+export default config
