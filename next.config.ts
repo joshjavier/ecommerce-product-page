02eadd7faf27ba05c740a48bd82159331fcbfe70
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-08-23 18:19:47 +0800

    chore: install and configure `@svgr/webpack`

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              svgoConfig: {
+                plugins: ['removeDimensions'],
+              },
+            },
+          },
+        ],
+        as: '*.js',
+      },
+    },
+  },
 }
 
 export default nextConfig
